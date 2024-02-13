a7f8c70080443861352f8608f32266115146bd07
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 00:20:38 +0530

    commit 14/2/24

---
+++ 6. Operators/logicalOperator.java
@@ -0,0 +1,15 @@
+public class logicalOperator {
+    public static void main(String[] args) {
+
+        // Logical &&
+
+        System.out.println((3>2)&&(5>1));
+
+        // Locical OR
+
+        System.out.println((3>2)||(5<1));
+
+        // Logical NOT
+        System.out.println(!(3>2));
+    }
+}
