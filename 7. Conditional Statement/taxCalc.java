3e3d5f65a02af190e157fb4376c9d29166ebd51a
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 01:16:44 +0530

    commit 14/2/24

---
+++ 7. Conditional Statement/taxCalc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class taxCalc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter income :- ");
+        int income = sc.nextInt();
+        int tax;
+        
+        // System.out.print("Enter Tax :- ");
+        // int tax = sc.nextInt();
+
+        if (income < 500000){
+            tax = 0;
+        }
+        else if(income >= 500000 && income < 1000000){
+            tax = (int) (income * 0.2);
+        }
+        else{
+            tax = (int) (income * 0.3);
+        }
+
+        System.out.println("Your tax :- " + tax);
+        
+    }
+}
