93101da517517b4e381603b5abce0071f93c16f3
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-13 23:29:11 +0530

    commit 13/2/24

---
+++ circleArea.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class circleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter Radius");
+        float rad = sc.nextFloat();
+        
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+}
