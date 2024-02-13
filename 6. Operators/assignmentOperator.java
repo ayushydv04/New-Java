5fc89ea57683ac451efed0fce0c16763a82071ba
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 00:29:03 +0530

    commit 14/2/24

---
+++ 6. Operators/assignmentOperator.java
@@ -0,0 +1,17 @@
+public class assignmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        // a = a + 10;
+        a+=10;
+        int b = 5;
+        b-=5;
+        int c = 2;
+        c*=2;
+        int d = 10;
+        d/=2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
