da04065acb149964a56e06361e40961ba6d8fffe
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 01:56:03 +0530

    commit 14/2/24

--- 8. Loops/squarePattern.java
+++ 8. Loops/squarePattern.java
@@ -1,5 +1,16 @@
-package 8. Loops;
 
 public class squarePattern {
-    
+    public static void main(String[] args) {
+        
+    //     for(int i = 1; i <=4; i++ ){
+    //         System.out.println("* * * *");
+    //     }
+
+    int line = 1;
+    while(line<=4){
+        System.out.println("* * * *");
+        line++;
+    }
+
+    }
 }
