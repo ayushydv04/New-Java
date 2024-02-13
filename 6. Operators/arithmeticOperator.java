b7348b0fe153741939ccaf62941ebc237f836b41
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 00:08:55 +0530

    commit 14/2/24

---
+++ 6. Operators/arithmeticOperator.java
@@ -0,0 +1,20 @@
+public class arithmeticOperator {
+    public static void main(String[] args) {
+        /*
+         * + --> for sum
+         * - --> for substraction
+         * * --> for multiplication
+         * / --> for division
+         * % --> gives remainder
+         */
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Add = " + (a+b));
+        System.out.println("Sub = " + (a-b));
+        System.out.println("Mul = " + (a*b));
+        System.out.println("Div = " + (a/b));
+        System.out.println("Mod = " + (a%b));
+    }
+}
