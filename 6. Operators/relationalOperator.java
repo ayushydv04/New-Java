792b621e3c0e006a9eabb513acd582d4fda26cdb
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 00:14:43 +0530

    commit 14/2/24

---
+++ 6. Operators/relationalOperator.java
@@ -0,0 +1,13 @@
+
+public class relationalOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 1;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+    }
+}
