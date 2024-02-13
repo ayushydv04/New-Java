b5187ad31ec80097a606d95f1d41a3866c24393a
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 00:44:13 +0530

    commit 14/2/24

---
+++ 7. Conditional Statement/printLargestOfTwonos.java
@@ -0,0 +1,14 @@
+public class printLargestOfTwonos {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+
+        if(a>=b){
+            System.out.println("A is largest of two nos");
+        }
+        else{
+            System.out.println("B is largest of two nos");
+        }
+    }
+}
