12f8d5662e39d135fc6981005d561e669d4f44ea
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-15 00:17:08 +0530

    commit 15/2/24

--- 12. Array/arrayBasics.java
+++ 12. Array/arrayBasics.java
@@ -1,5 +1,13 @@
 public class arrayBasics {
     public static void main(String[] args) {
-        
+        // Way -1
+        int marks[] = new int[50];
+        // Way-2
+        int numbers[] = {1, 2, 3};
+        // Way-3
+        int moreNumbers[] = {4, 5, 6};
+        // Way-4
+        String fruits[] = {"apple", "mango", "orange"};
+
     }
 }
