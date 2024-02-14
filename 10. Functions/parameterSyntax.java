089b90efa6c88d8aeb3f35c089ff719ff85cf026
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 22:46:32 +0530

    commit 14/2/24

--- 10. Functions/parameterSyntax.java
+++ 10. Functions/parameterSyntax.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class parameterSyntax {
 
-    public static int CalcSum(int n1, int n2){
+    public static int CalcSum(int n1, int n2){ // Parameters or formal parameters 
         int sum = n1 + n2;
         return sum;
 
@@ -12,7 +12,7 @@ public class parameterSyntax {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = CalcSum(a, b);
+        int sum = CalcSum(a, b); // arguments or actual parameters
         System.out.println("Sum :- " + sum);
     }
 }
