885d2bff6d94b9a0abea277aec8c24a671cb61c8
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-14 22:26:21 +0530

    Update funcBasics.java

--- 10. Functions/funcBasics.java
+++ 10. Functions/funcBasics.java
@@ -1,5 +1,11 @@
 public class funcBasics {
     public static void main(String[] args) {
+            // Syntax
+        // returnType name(){
+        //     //body
+        //     // return statement;
+        // }
+
         
     }
 }
