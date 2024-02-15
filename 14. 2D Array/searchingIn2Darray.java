40eb962183e497324954a604aebeb51944c9e1fd
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 02:47:58 +0530

    commit 16/2/24

--- 14. 2D Array/searchingIn2Darray.java
+++ 14. 2D Array/searchingIn2Darray.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
 public class searchingIn2Darray {
+
+    public static boolean search(int arr[][], int key){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                if(arr[i][j]==key){
+                    System.out.println("Found at cell (" + i + ", " + j + ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+
     public static void main(String[] args) {
         
         int arr[][] = new int[3][3];
+        int key = 5;
     
         Scanner sc = new Scanner(System.in);
     
@@ -22,6 +37,9 @@ public class searchingIn2Darray {
             }
             System.out.println();
         }
+
+        search(arr, key);
+
     }
 
 
