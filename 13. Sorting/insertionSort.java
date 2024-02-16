9e032551afdeb6b870c59e09c4e6656b356c062c
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-17 00:36:45 +0530

    commit 17/2/24

--- 13. Sorting/insertionSort.java
+++ 13. Sorting/insertionSort.java
@@ -1,27 +1,27 @@
 public class insertionSort {
 
-    public static void InsertionSort(int arr[]){
-        for(int i = 1; i<arr.length; i++){
-            int curr = arr[i];
-            int prev = i-1;
-            //finding out the correct pos to insert
-            while(prev >= 0 && arr[prev] > curr){
-                arr[prev+1] = arr[prev];
-                prev--;
+    public static void InsertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            // finding out the correct pos to insert
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
             }
-            //insertion
-            arr[prev+1] = curr;
+            // insertion
+            arr[j + 1] = temp;
         }
     }
 
-    public static void printArr(int arr[]){
-        for(int i = 0; i<arr.length-1; i++){
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = { 5, 4, 1, 3, 2 };
         InsertionSort(arr);
         printArr(arr);
     }
--- 15. Practice/insertionSort.java
+++ 15. Practice/insertionSort.java
@@ -1,11 +1,29 @@
 public class insertionSort {
 
-    public static void InseetionSort(int arr[]){
-        
+    public static void InsertionSort(int arr[]){
+        for(int i = 1; i<arr.length; i++){
+            int temp = arr[i];
+            int j = i-1;
+            // Finding correct pos to insert
+            while(j >= 0 && arr[j] > temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            // insertion
+            arr[j+1] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
     public static void main(String[] args) {
         int arr[] = {5, 4, 3, 2, 1};
+        InsertionSort(arr);
+        printArr(arr);
 
     }
 }
