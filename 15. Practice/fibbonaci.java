9c8660a64910c096a8c8d1c1a064e3116b7df4bd
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 19:33:43 +0530

    Create fibbonaci.java

---
+++ 15. Practice/fibbonaci.java
@@ -0,0 +1,12 @@
+public class fibbonaci {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+
+        int n;
+
+        while(i<=n){
+            
+        }
+    }
+}
