cf8b4b0756c4788ce81b5783ec9f2a5433091aee
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 23:36:00 +0530

    Create insertionSort.java

---
+++ 15. Practice/insertionSort.java
@@ -0,0 +1,11 @@
+public class insertionSort {
+
+    public static void InseetionSort(int arr[]){
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 2, 1};
+
+    }
+}
