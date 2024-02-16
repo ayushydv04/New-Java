cd20119d20d9dc2e483bbb71f36c2101bd27b5d1
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 22:32:47 +0530

    commit 16/2/24

--- 15. Practice/getLargest.java
+++ 15. Practice/getLargest.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class getLargest {
+
+    public static int Largest(int arr[]){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 3, 6, 10, 4, 89, 90, 34 };
+        int arr[] = {3, 6, 10, 4, 89, 90, 34, 100};
+        System.out.println(Largest(arr));
     }
 }
