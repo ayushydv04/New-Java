f7f1122b2649a226c335951d0138b0f64ffb2e44
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 19:31:32 +0530

    commit 16/2/24

---
+++ 15. Practice/oneton.java
@@ -0,0 +1,8 @@
+public class oneton {
+    public static void main(String[] args) {
+        int n = 6;
+        for(int i = 1; i <= n; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
