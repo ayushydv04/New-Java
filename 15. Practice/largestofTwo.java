9670415eeba4cb4ffa81e8333f0554397ae2be79
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 19:25:24 +0530

    commit 16/2/24

---
+++ 15. Practice/largestofTwo.java
@@ -0,0 +1,11 @@
+public class largestofTwo {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 31;
+        if(a>b){
+            System.out.println(a + " a is Largest of two");
+        }else{
+            System.out.println(b + " b is largest of two");
+        }
+    }
+}
