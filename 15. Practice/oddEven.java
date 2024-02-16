155a3277bf5de750d84d94ae783f1360f08fede5
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 19:22:15 +0530

    commit 16/2/24

---
+++ 15. Practice/oddEven.java
@@ -0,0 +1,10 @@
+public class oddEven {
+    public static void main(String[] args) {
+        int n = 121;
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+}
