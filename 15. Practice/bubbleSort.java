62025a20b04227dd2e77d4c0a4f6d92d5cdb8612
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 23:10:34 +0530

    commit 16/2/24

--- 15. Practice/bubbleSort.java
+++ 15. Practice/bubbleSort.java
@@ -1,8 +1,8 @@
 public class bubbleSort {
 
     public static void BubbleSort(int arr[]){
-        for(int i = 0; i<arr.length-1; i++){
-            for(int j = 0; j<arr.length-1 - i; j++){
+        for(int i = 1; i<arr.length; i++){
+            for(int j = 0; j<arr.length - i; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
