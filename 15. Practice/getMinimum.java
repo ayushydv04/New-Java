8e115c46026d02c0c0140e7e4bbc4afcc5ccaa67
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 22:38:52 +0530

    commit 16/2/24

--- 15. Practice/getMinimum.java
+++ 15. Practice/getMinimum.java
@@ -1,8 +1,18 @@
+import java.util.*;
 public class getMinimum {
 
-    
+    public static int MinElement(int arr[]){
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 
     public static void main(String[] args) {
-        
+        int arr[] = {3, 6, 10, 4, 89, 1, 90, -3, 34, 100};
+        System.out.println(MinElement(arr));
     }
 }
