80c6d431b81a2fe78fa4c7761dc7c5a597f84563
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 20:06:20 +0530

    commit 16/2/24

---
+++ 15. Practice/prod.java
@@ -0,0 +1,12 @@
+public class prod {
+
+    public static int Prod(int a, int b){
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        // int p = 
+        System.out.println(Prod(10,20));
+        
+    }
+}
