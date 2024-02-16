0e8a7e0eea063f2f5685f2bd92f12732af58f1ba
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 23:26:03 +0530

    commit 16/2/24

--- 15. Practice/selectionSort.java
+++ 15. Practice/selectionSort.java
@@ -1,19 +1,16 @@
-// Selection sort mei hm pehle first element ko min maan lete h aur fir pure array m check krte h agr usse bhi koi chota element milta h toh hm fir usko min bna dete h aur fir i ko min se swap kr dete h
-
 public class selectionSort {
 
     public static void SelectionSort(int arr[]){
         for(int i = 0; i<arr.length-1; i++){
             int min = i;
-            for(int j = i+1; j < arr.length; j++){
+            for(int j = i+1; j<arr.length; j++){
                 if(arr[min] > arr[j]){
                     min = j;
                 }
             }
-            // Swap
             int temp = arr[min];
             arr[min] = arr[i];
-            arr[i] = temp;
+            arr[i] = temp; 
         }
     }
 
@@ -24,8 +21,9 @@ public class selectionSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = {5, 4, 2, 3, 1};
         SelectionSort(arr);
         printArr(arr);
+
     }
 }
