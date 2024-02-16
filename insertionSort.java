15c367fa864bc6e5c54be2fff44a71c8ccc8a7e9
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-17 00:51:12 +0530

    commit 17/2/24

---
+++ insertionSort.java
@@ -0,0 +1,26 @@
+public class insertionSort {
+
+    public static void InsertionSort(int arr[]){
+        for(int i = 1; i<arr.length; i++){
+            int temp = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 2, 1};
+        InsertionSort(arr);
+        printArr(arr);
+    }
+}
