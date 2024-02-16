a6c567cbec64545cf033e7bf0f51a72cfce08c2e
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-16 19:40:12 +0530

    commit 16/2/24

--- 15. Practice/fibbonaci.java
+++ 15. Practice/fibbonaci.java
@@ -2,11 +2,14 @@ public class fibbonaci {
     public static void main(String[] args) {
         int a = 0;
         int b = 1;
+        int next;
+        int n = 8;
 
-        int n;
-
-        while(i<=n){
-            
+        for(int i = 1; i<=n; i++){
+            System.out.print(a + " ");
+            next = a + b;
+            a = b;
+            b = next;
         }
     }
 }
