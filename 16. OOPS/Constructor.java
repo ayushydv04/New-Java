dee30735f1d3bb37e7986668ad28ec3d3bebe881
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-03-22 23:44:01 +0530

    commit 22/3/24

---
+++ 16. OOPS/Constructor.java
@@ -0,0 +1,15 @@
+public class Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student("Ayush");
+        System.out.println(s1.name);
+    }    
+}
+
+class Student{
+    String name;
+    int marks;
+
+    Student(String name){
+        this.name = name;
+    }
+}
